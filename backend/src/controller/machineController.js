7bec11f9a9c8e97d40f5adc814b341074e7f30da
honey4251996 <peacehoney425@gmail.com>
2020-07-27 07:50:00 +0800

    working with update machine API

--- backend/src/controller/machineController.js
+++ backend/src/controller/machineController.js
@@ -23,7 +23,53 @@ const machineController = {
         database.query(query, [processId])
             .then(result => returnResult(res, 'Machine by id', 0, result))
             .catch(err => returnResult(res, 'Oops, error in getting machine!', 1, err));
-    }
+    },
+
+    updateMachine(req, res) {
+        let businessUnity = req.body.raw.BusinessUnity == '' ? 0 : req.body.raw.BusinessUnity,
+            name = req.body.raw.Name,
+            oldId = req.body.raw.oldId,
+            machineCode = req.body.raw.MachineCode,
+            processId = req.body.raw.ProcessId,
+            quality = req.body.raw.Quality == '' ? 0 : req.body.raw.Quality,
+            eficiency = req.body.raw.Eficiency == '' ? 0 : req.body.raw.Eficiency,
+            availability = req.body.raw.Availability == '' ? 0 : req.body.raw.Availability,
+            setupTime = req.body.raw.SetupTime == '' ? 0 : req.body.raw.SetupTime,
+            setupTimeUnity = req.body.raw.SetupTimeUnity,
+            cost = req.body.raw.Cost == '' ? 0 : req.body.raw.Cost,
+            costTimeUnity = req.body.raw.CostTimeUnity,
+            setupLoss = req.body.raw.SetupLoss == '' ? 0 : req.body.raw.SetupLoss,
+            speed = req.body.raw.Speed == '' ? 0 : req.body.raw.Speed,
+            speedUnity = req.body.raw.SpeedUnity,
+            speedTimeUnity = req.body.raw.SpeedTimeUnity,
+            minBatch = req.body.raw.MinBatch == '' ? 0 : req.body.raw.MinBatch,
+            minBatchUnity = req.body.raw.MinBatchUnity,
+            maxBatch = req.body.raw.MaxBatch == '' ? 0 : req.body.raw.MaxBatch,
+            maxBatchUnity = req.body.raw.MaxBatchUnity,
+            groupSpeed = req.body.raw.GroupSpeed == '' ? 0 : req.body.raw.GroupSpeed,
+            groupSpeedTimeUnity = req.body.raw.GroupSpeedTimeUnity,
+            groupSpeedUnity = req.body.raw.GroupSpeedUnity,
+            groupName = req.body.raw.GroupName,
+            setupLossUnity = req.body.raw.SetupLossUnity,
+            onOff = req.body.raw.OnOff,
+            shiftA = req.body.raw.ShiftA,
+            shiftB = req.body.raw.ShiftB,
+            shiftC = req.body.raw.ShiftC,
+            shiftD = req.body.raw.ShiftD,
+            shiftE = req.body.raw.ShiftE,
+            extraFields = JSON.stringify(req.body.raw.EXTRAFIELDS);
+
+        let query = 'update  processrecord SET BusinessUnity=?,Name=?,MachineCode=?,ProcessId=?,Quality=?,Eficiency=?,Availability=?,SetupTime=?,SetupTimeUnity=?,Cost=?,CostTimeUnity=?,SetupLoss=?,SetupLossUnity=?,Speed=?,SpeedUnity=?,SpeedTimeUnity=?,MinBatch=?,MinBatchUnity=?,MaxBatch=?,MaxBatchUnity=?,GroupSpeed=?,GroupSpeedTimeUnity=?,GroupSpeedUnity=?,GroupName=?,OnOff=?,EXTRAFIELDS=?,ShiftA=?,ShiftB=?,ShiftC=?,ShiftD=?,ShiftE=? where id=?';
+        let params = [businessUnity, name, machineCode, processId, quality, eficiency, availability,
+            setupTime, setupTimeUnity, cost, costTimeUnity, setupLoss, setupLossUnity, speed,
+            speedUnity, speedTimeUnity, minBatch, minBatchUnity, MaxBatch, MaxBatchUnity, GroupSpeed, GroupSpeedTimeUnity,
+            groupSpeedUnity, groupName, onOff, extraFields, shiftA, shiftB, shiftC, shiftD, shiftE, oldId]
+        database.query(query, params)
+            .then(result => {
+                returnResult(res, 'Updated Successfully!', 0, result);
+            })
+            .catch(err => returnResult(res, 'Oops, Error in updating machine!', 1, err));
+    },
 
 
 
--- backend/src/routes/machineRoute.js
+++ backend/src/routes/machineRoute.js
@@ -8,5 +8,6 @@ export default  (app) => {
   router.get('/api/getProcessMeachne', controller.machineController.getProcessAndMachine);
   router.get('/api/getProcessRecord/:id', controller.machineController.getMachineByProcessId);
   router.get('/api/getProcessRecordByid/:id', controller.machineController.getMachineById);
+  router.post('/api/updateProcessrecord', controller.machineController.updateMachine);
 
 };
